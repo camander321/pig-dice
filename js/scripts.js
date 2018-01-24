4e7af6b0119729a593bf36012f43831e50d02a2a
Cameron Anderson and Kaila Goff <cameron@>
2018-01-24 15:30:32 -0800

    refactor code into more objects

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@ function Player(name) {
   this.score = 0;
   this.running = 0;
   this.total = 0;
+  this.behavior = function(){};
 }
 
 Player.prototype.addRunning = function (amount) {
@@ -10,62 +11,70 @@ Player.prototype.addRunning = function (amount) {
   this.total = this.score + this.running;
 };
 
-Player.prototype.addScore = function () {
+Player.prototype.hold = function () {
   this.score += this.running;
   this.total = this.score;
   this.running = 0;
 };
 
-function switchTurn(player1, player2, currentPlayer) {
-  var nextPlayer;
-  if (currentPlayer === player1) {
-    nextPlayer = player2;
-  } else {
-    nextPlayer = player1;
+Player.prototype.loseTurn = function() {
+  this.running = 0;
+  this.total = this.score;
+};
+
+
+function Game() {}
+
+Game.prototype.setPlayers = function (player1, player2) {
+  this.player1 = player1;
+  this.player2 = player2;
+  this.currentPlayer = this.player1
+};
+
+Game.prototype.switchTurn = function() {
+  if (this.currentPlayer === this.player1)
+    this.currentPlayer = this.player2;
+  else
+    this.currentPlayer = this.player1;
+
+  if (this.currentPlayer.name === "computer") {
+    // do computer stuff
+    console.log(this.currentPlayer)
+    this.currentPlayer.behavior(this);
+    this.switchTurn();
   }
-  console.log("Switching to: " + nextPlayer.name + "'s turn");
-
-  if (nextPlayer.name === "computer") {
-
-    for (var i = 0; i < 3 || nextPlayer.running <= 15; i++) {
-      if (!roll(nextPlayer))
-        break;
-    }
-
-
-    nextPlayer.addScore();
-    updateScores(player1, player2);
-    if (nextPlayer.total >= 100) {
-      $("#result").text(nextPlayer.name + " WINS!!!");
-      $("#result").fadeIn();
-      $("#game").hide();
-      return nextPlayer;
-    }
-    return switchTurn(player1, player2, nextPlayer);
-  }
-  return nextPlayer;
 }
 
-function roll(player) {
+Game.prototype.roll = function() {
   var number = Math.floor(Math.random() * Math.floor(6)) + 1;
-  console.log(player.name + " " + number);
-  $("#showRoll").text(number);
-  if (number === 1) {
-    player.running = 0;
-    player.total = player.score;
-    return false;
-  }
-  player.addRunning(number);
-  return true;
-}
+  console.log(this.currentPlayer.name + " " + number);
 
-function updateScores(player1, player2) {
-  $(".p1Running").text(player1.running);
-  $(".p1Score").text(player1.score);
-  $(".p1Total").text(player1.total);
-  $(".p2Running").text(player2.running);
-  $(".p2Score").text(player2.score);
-  $(".p2Total").text(player2.total);
+  if (number === 1) {
+    this.currentPlayer.loseTurn();
+    this.switchTurn();
+    updateScores(this);
+    return false;
+  } else {
+
+    this.currentPlayer.addRunning(number);
+    updateScores(this);
+    return true;
+  }
+};
+
+Game.prototype.hold = function() {
+  this.currentPlayer.hold();
+  updateScores(this);
+  this.switchTurn();
+};
+
+function updateScores(game) {
+  $(".p1Running").text(game.player1.running);
+  $(".p1Score").text(game.player1.score);
+  $(".p1Total").text(game.player1.total);
+  $(".p2Running").text(game.player2.running);
+  $(".p2Score").text(game.player2.score);
+  $(".p2Total").text(game.player2.total);
 }
 
 function animateDice() {
@@ -83,43 +92,74 @@ function animateDice() {
   }
 }
 
+function easyMode(game) {
+  for (var i = 0; i < 3; i++) {
+    if (!game.roll())
+      break;
+    game.hold();
+  }
+}
+
 
 $(document).ready(function() {
+  var game;
 
-  var player1;
-  var player2;
-  var player;
   $("#start-btn").click(function () {
+    game = new Game();
     player1 = new Player($("#p1Mode").val());
     player2 = new Player($("#p2Mode").val());
+    if($("#p1Mode").val() === "computer") {
+      $("#p1roll").prop("disabled",true);
+      $("#p1hold").prop("disabled",true);
+      player1.behavior = easyMode;
+    }
+    if($("#p2Mode").val() === "computer") {
+      $("#p2roll").prop("disabled",true);
+      $("#p2hold").prop("disabled",true);
+      player2.behavior = easyMode;
+    }
+
+    game.setPlayers(player1, player2);
     $(".start").hide();
     $("#game").fadeIn();
-    player = player1;
+  });
+
+  $("#roll").click(function() {
+    game.roll();
+  });
+
+  $("#hold").click(function() {
+    game.hold();
   });
 
 
-  $("#roll").click(function() {
-    animateDice();
-    var isOne = !roll(player);
-    if (isOne) {
-      player = switchTurn(player1, player2, player);
-    }
-
-    updateScores(player1, player2);
-
-    if (player.total >= 100) {
-      $("#result").text(player.name + " WINS!!!");
-      $("#result").fadeIn();
-      $("#game").hide();
-      return;
-    }
 
 
-  })
-  $("#hold").click(function() {
-    player.addScore();
-    updateScores(player1, player2);
-    player = switchTurn(player1, player2, player);
-  })
+
+
+
+  // $("#roll").click(function() {
+  //   animateDice();
+  //   var isOne = !roll(player);
+  //   if (isOne) {
+  //     player = switchTurn(player1, player2, player);
+  //   }
+  //
+  //   updateScores(player1, player2);
+  //
+  //   if (player.total >= 100) {
+  //     $("#result").text(player.name + " WINS!!!");
+  //     $("#result").fadeIn();
+  //     $("#game").hide();
+  //     return;
+  //   }
+  //
+  //
+  // })
+  // $("#hold").click(function() {
+  //   player.addScore();
+  //   updateScores(player1, player2);
+  //   player = switchTurn(player1, player2, player);
+  // })
 
 });
